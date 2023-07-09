5431446f65b52e6e8c288d12890fef7978225e21
canberkelmal <elmalcanberk@gmail.com>
2023-07-09 16:26:34 +0300

    Better blowing

--- Assets/Scripts/LineConnector.cs
+++ Assets/Scripts/LineConnector.cs
@@ -5,10 +5,12 @@ using UnityEngine;
 public class LineConnector : MonoBehaviour
 {
     public float animSens = 1f;
+    public float animWidth = 0.9f;
     public GameObject[] _objs;
 
     private LineRenderer line;
     private float tempT = 0.02f;
+    private float tempW = 0.25f;
     private CollectableSc currentCollectable;
     AnimationCurve curve = new AnimationCurve();
 
@@ -37,11 +39,20 @@ public class LineConnector : MonoBehaviour
 
     private void PipeGetAnim()
     {
-        tempT += Time.deltaTime * animSens;
-
-        if (tempT < 0.95f)
+        if (tempT < 0.95f && line.widthCurve.keys[1].value < animWidth)
         {
-            MoveKeyFrame(1, tempT, 0.75f);
+            tempW = Mathf.MoveTowards(line.widthCurve.keys[1].value, animWidth, animSens * Time.deltaTime * 1.5f);
+            MoveKeyFrame(1, 0.05f, tempW);
+        }
+        else if (tempT < 0.95f)
+        {
+            tempT += Time.deltaTime * animSens;
+            MoveKeyFrame(1, tempT, animWidth);
+        }
+        else if (line.widthCurve.keys[1].value > 0.25f)
+        {
+            tempW = Mathf.MoveTowards(line.widthCurve.keys[1].value, 0.25f, animSens * Time.deltaTime * 1.5f);
+            MoveKeyFrame(1, tempT, tempW);
         }
         else
         {
@@ -49,22 +60,6 @@ public class LineConnector : MonoBehaviour
             currentCollectable.TakeTheFruit();
             CancelInvoke("PipeGetAnim");
         }
-        /*curve = new AnimationCurve();
-        if (tempT < 0.95f)
-        {
-            curve.AddKey(0, 0.25f);
-            curve.AddKey(tempT, 0.75f);
-            curve.AddKey(1, 0.25f);
-            line.widthCurve = curve;
-        }
-        else
-        {
-            curve.AddKey(0, 0.25f);
-            curve.AddKey(1, 0.25f);
-            line.widthCurve = curve;
-            currentCollectable.TakeTheFruit();
-            CancelInvoke("PipeGetAnim");
-        }*/
     }
     private void MoveKeyFrame(int index, float newTime, float newValue)
     {
@@ -76,88 +71,5 @@ public class LineConnector : MonoBehaviour
         curve.keys = keyFrames;
         line.widthCurve = curve;
     }
-
-    /*private void PipeGetAnim()
-    {
-        if ( currentKey == 1)
-        {
-            _objs[currentKey].transform.localScale += Vector3.one * animSens * Time.deltaTime;
-            _objs[currentKey + 1].transform.localScale += Vector3.one * animSens * Time.deltaTime / 2;
-            if (_objs[currentKey].transform.localScale.x >= 0.5)
-            {
-                _objs[currentKey].transform.localScale = Vector3.one * 0.5f;
-                _objs[currentKey + 1].transform.localScale = Vector3.one * 0.3f;
-                currentKey++;
-            }
-        }
-        else if (currentKey > 1 && currentKey < _objs.Length-2)
-        {
-            _objs[currentKey - 1].transform.localScale -= Vector3.one * animSens * Time.deltaTime;
-            _objs[currentKey].transform.localScale += Vector3.one * animSens * Time.deltaTime;
-            _objs[currentKey + 1].transform.localScale += Vector3.one * animSens * Time.deltaTime / 2;
-            if (_objs[currentKey].transform.localScale.x >= 0.5)
-            {
-                _objs[currentKey - 1].transform.localScale = Vector3.one * 0.1f;
-                _objs[currentKey].transform.localScale = Vector3.one * 0.5f;
-                _objs[currentKey + 1].transform.localScale = Vector3.one * 0.3f;
-                currentKey++;
-            }
-
-        }
-        else if (currentKey == _objs.Length - 2 && _objs[currentKey-1].transform.localScale.x > 0.1f)
-        {
-            _objs[currentKey - 1].transform.localScale -= Vector3.one * animSens * Time.deltaTime;
-            _objs[currentKey].transform.localScale += Vector3.one * animSens * Time.deltaTime;
-        }
-        else if (currentKey == _objs.Length - 2 && _objs[currentKey - 1].transform.localScale.x <= 0.1f)
-        {
-            _objs[currentKey].transform.localScale -= Vector3.one * animSens * Time.deltaTime;
-            if (_objs[currentKey].transform.localScale.x <= 0.1f)
-            {
-                _objs[currentKey - 1].transform.localScale = Vector3.one * 0.1f;
-                _objs[currentKey].transform.localScale = Vector3.one * 0.1f;
-                currentKey = 1;
-                currentCollectable.TakeTheFruit();
-                CancelInvoke("PipeGetAnim");
-            }
-        }
-    }*/
-    /*private void PipeGetAnim()
-    {
-        Debug.Log(line.widthCurve.keys[currentKey].value);
-        if (currentKey == 0)
-        {
-            line.widthCurve.keys[currentKey].value = Mathf.MoveTowards(0.25f, 0.75f, animSens * Time.deltaTime);
-            if (line.widthCurve.keys[currentKey].value == 0.75)
-            {
-                currentKey++;
-            }
-        }
-        else if (currentKey > 0 && currentKey < line.widthCurve.keys.Length - 1)
-        {
-            line.widthCurve.keys[currentKey].value = Mathf.MoveTowards(0.25f, 0.75f, animSens * Time.deltaTime);
-            line.widthCurve.keys[currentKey - 1].value = Mathf.MoveTowards(0.75f, 0.25f, animSens * Time.deltaTime);
-            if (line.widthCurve.keys[currentKey].value == 0.75)
-            {
-                currentKey++;
-            }
-
-        }
-        else if (currentKey == line.widthCurve.keys.Length - 1 && line.widthCurve.keys[currentKey - 1].value > 0.25f)
-        {
-            line.widthCurve.keys[currentKey].value = Mathf.MoveTowards(0.25f, 0.75f, animSens * Time.deltaTime);
-            line.widthCurve.keys[currentKey - 1].value = Mathf.MoveTowards(0.75f, 0.25f, animSens * Time.deltaTime);
-        }
-        else if (currentKey == line.widthCurve.keys.Length - 1 && line.widthCurve.keys[currentKey - 1].value == 0.25f)
-        {
-            line.widthCurve.keys[currentKey].value = Mathf.MoveTowards(0.75f, 0.25f, animSens * Time.deltaTime);
-            if (line.widthCurve.keys[currentKey].value == 0.25)
-            {
-                currentKey = 0;
-                currentCollectable.TakeTheFruit();
-                CancelInvoke("PipeGetAnim");
-            }
-        }
-    }*/
 
 }
