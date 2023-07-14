432b8171adb8bac8901d5962b091647913e3bc0f
canberkelmal <elmalcanberk@gmail.com>
2023-07-14 17:00:48 +0300

    Final tank created

--- Assets/FinalCupSc.cs
+++ Assets/FinalCupSc.cs
@@ -11,13 +11,24 @@ public class FinalCupSc : MonoBehaviour
     }
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("VacuumArea"))
+        if (other.CompareTag("VacuumArea") && !gameManager.isTankEmpty)
         {
-            Vector3 cupScreenPos = Camera.main.WorldToScreenPoint(transform.position);
-            Instantiate(gameManager.cupIcon, cupScreenPos, Quaternion.identity, gameManager.cupCountTx.transform.parent);
+            if(!gameManager.isTankEmpty)
+            {
+                Vector3 cupScreenPos = Camera.main.WorldToScreenPoint(transform.position);
+                Instantiate(gameManager.cupIcon, cupScreenPos, Quaternion.identity, gameManager.cupCountTx.transform.parent);
+                //Destroy(Instantiate(gameManager.getCupParticle, transform.position, Quaternion.identity),1f);
 
-            gameManager.FillACup();
-            Destroy(gameObject);
+                GameObject getEffect = Instantiate(gameManager.getJuiceParticle, transform.position + Vector3.up, Quaternion.identity);
+                getEffect.GetComponent<ParticleSystem>().startColor = gameManager.liquidColor;
+                getEffect.transform.GetChild(0).GetComponent<ParticleSystem>().startColor = gameManager.liquidColor;
+                getEffect.transform.GetChild(1).GetComponent<ParticleSystem>().startColor = gameManager.liquidColor;
+                Destroy(getEffect,1f);
+
+                gameManager.FillACup();
+                transform.GetChild(0).gameObject.SetActive(true);
+                Destroy(gameObject, gameManager.getCupDelay);
+            }
         }
     }
 }
