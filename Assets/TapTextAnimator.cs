ac14ee7efa4bdabd742be069b9ee858920623c19
canberkelmal <elmalcanberk@gmail.com>
2023-07-19 19:35:00 +0300

    Tap to start text done

---
+++ Assets/TapTextAnimator.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TapTextAnimator : MonoBehaviour
+{
+    public Vector2 limits = new Vector2(-700, -715);
+    public float sens = 1;
+    public float offset = 2;
+
+    private bool dir = false;
+
+    private void Update()
+    {
+        Vector3 target = dir ? Vector3.up * (limits.x + offset) : Vector3.up * (limits.y - offset);
+        transform.localPosition = Vector3.MoveTowards(transform.localPosition, target, sens * Time.deltaTime);
+
+        if(dir && transform.localPosition.y > limits.x)
+        {
+            dir = false;
+        }
+        else if(!dir && transform.localPosition.y < limits.y)
+        {
+            dir = true;
+        }
+
+        if (Input.GetMouseButton(0))
+        {
+            Destroy(gameObject);
+        }
+    }
+}
