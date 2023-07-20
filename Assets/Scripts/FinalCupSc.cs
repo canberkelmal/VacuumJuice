9f1254ece53dd6b3f2ba4adbf69d6ee381cfe30a
canberkelmal <elmalcanberk@gmail.com>
2023-07-20 20:24:04 +0300

    Better scripts

--- Assets/Scripts/FinalCupSc.cs
+++ Assets/Scripts/FinalCupSc.cs
@@ -13,22 +13,19 @@ public class FinalCupSc : MonoBehaviour
     {
         if (other.CompareTag("VacuumArea") && !gameManager.isTankEmpty)
         {
-            if(!gameManager.isTankEmpty)
-            {
-                Vector3 cupScreenPos = Camera.main.WorldToScreenPoint(transform.position);
-                Instantiate(gameManager.cupIcon, cupScreenPos, Quaternion.identity, gameManager.cupCountTx.transform.parent);
-                //Destroy(Instantiate(gameManager.getCupParticle, transform.position, Quaternion.identity),1f);
+            Vector3 cupScreenPos = Camera.main.WorldToScreenPoint(transform.position);
+            Instantiate(gameManager.cupIcon, cupScreenPos, Quaternion.identity, gameManager.cupCountTx.transform.parent);
+            //Destroy(Instantiate(gameManager.getCupParticle, transform.position, Quaternion.identity),1f);
 
-                GameObject getEffect = Instantiate(gameManager.getJuiceParticle, transform.position + Vector3.up, Quaternion.identity);
-                getEffect.GetComponent<ParticleSystem>().startColor = gameManager.liquidColor;
-                getEffect.transform.GetChild(0).GetComponent<ParticleSystem>().startColor = gameManager.liquidColor;
-                getEffect.transform.GetChild(1).GetComponent<ParticleSystem>().startColor = gameManager.liquidColor;
-                Destroy(getEffect,1f);
+            GameObject getEffect = Instantiate(gameManager.getJuiceParticle, transform.position + Vector3.up, Quaternion.identity);
+            getEffect.GetComponent<ParticleSystem>().startColor = gameManager.liquidColor;
+            getEffect.transform.GetChild(0).GetComponent<ParticleSystem>().startColor = gameManager.liquidColor;
+            getEffect.transform.GetChild(1).GetComponent<ParticleSystem>().startColor = gameManager.liquidColor;
+            Destroy(getEffect, 1f);
 
-                gameManager.FillACup();
-                transform.GetChild(0).gameObject.SetActive(true);
-                Destroy(gameObject, gameManager.getCupDelay);
-            }
+            gameManager.FillACup();
+            transform.GetChild(0).gameObject.SetActive(true);
+            Destroy(gameObject, gameManager.getCupDelay);
         }
     }
 }
