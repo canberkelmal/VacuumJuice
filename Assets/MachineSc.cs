a4efe0804d326ca228927b571e85999a229c9e6d
canberkelmal <elmalcanberk@gmail.com>
2023-07-26 20:23:20 +0300

    Improving machine script

--- Assets/MachineSc.cs
+++ Assets/MachineSc.cs
@@ -1,19 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MachineSc : MonoBehaviour
 {
     // 0 No resource, 1 Preparing, 2 Ready
     public int status = 0;
     public float prepareDuration = 1;
+    public float resourceCount = 0;
+    public Color readyColor, unreadyColor;
 
     private float timer = 0;
+    private Image fillImage;
+    private GameObject statuIcon;
 
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        
+        fillImage = transform.Find("MachineCanvas").Find("BG").GetComponent<Image>();
+        statuIcon = transform.Find("MachineCanvas").Find("Statu").gameObject;
+        SetStatus(2);
+    }
+
+    public void PrepareTest()
+    {
+        if(status != 1)
+        {
+            resourceCount++;
+            SetStatus(1);
+        }
     }
 
     public void SetStatus(int stat)
@@ -21,35 +37,77 @@ public class MachineSc : MonoBehaviour
         status = stat;
         switch (status)
         {
-            case 0:
-                // No resource
+            case 0: // No resource
+                statuIcon.SetActive(false);
+
+                fillImage.color = unreadyColor;
                 break;
 
             case 1: // Preparing
+                statuIcon.SetActive(false);
+
                 timer = 0;
-                InvokeRepeating("PrepareProduct", 0, Time.deltaTime);
+                resourceCount--;
+                InvokeRepeating("PrepareProductLoop", 0, Time.deltaTime);
                 
                 break;
 
-            case 2:
-                // Ready
+            case 2: // Ready
+                statuIcon.SetActive(true);
+
+                fillImage.color = readyColor;
                 break;
         }
     }
 
-    private void PrepareProduct()
+    private void PrepareProductLoop()
     {
         timer += Time.deltaTime;
-        if(timer >= prepareDuration && status == 1)
+        float x = timer / prepareDuration;
+
+        fillImage.fillAmount = x;
+        fillImage.color = Color.Lerp(unreadyColor, readyColor, x);
+
+        if(x >= 1 && status == 1)
         {
+            if(resourceCount <= 0)
+            {
+                resourceCount = 0;
+            }
             ProductPrepared();
-            CancelInvoke("PrepareProduct");
+            CancelInvoke("PrepareProductLoop");
         }
     }
 
     private void ProductPrepared()
     {
         SetStatus(2);
+    }
+    public void TakeProduct()
+    {
+        // Take product
+        if (status == 2)
+        {
+            // Prepare new one
+            if (resourceCount > 0)
+            {
+                SetStatus(1);
+            }
+            // No more resources
+            else if (resourceCount == 0)
+            {
+                Debug.Log("Taken and no more resources.");
+                SetStatus(0);
+            }
+        }
+        else if(status == 1)
+        {
+            Debug.Log("Product is being prepared.");
+        }
+        else if(status == 0)
+        {
+            Debug.Log("No resource.");
+        }
     }
 
 
