5ea868e4a3ba2d4caa679ae47c05a3b68e1e10fb
canberkelmal <elmalcanberk@gmail.com>
2023-07-27 23:59:13 +0300

    Costumer script is roughly ready.

--- Assets/Scripts/IdleManager.cs
+++ Assets/Scripts/IdleManager.cs
@@ -1,32 +1,57 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class IdleManager : MonoBehaviour
 {
     public GameObject machinesParent;
+    public GameObject workersParent;
     public GameObject costumersParent;
     public GameObject costumer;
     public Transform costumerSpawnPoint;
     public Transform costumerLastPoint;
+    public Transform costumerExitPoint;
 
     private int costumerCount = 0;
     private GameObject[] machines;
+    private GameObject[] workers = new GameObject[0];
     private GameObject[] costumers = new GameObject[0];
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
         FillMachinesArray();
+        FillWorkersArray();
         costumerCount = costumersParent.transform.childCount;
+    }
+
+    private void Update()
+    {
+
+        // Restart the game when the "R" key is pressed
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            Restart();
+        }
     }
 
     private void FillMachinesArray()
     {
-        machines = new GameObject[machinesParent.transform.childCount-1];
+        machines = new GameObject[machinesParent.transform.childCount];
 
         for(int i = 0; i< machines.Length; i++)
         {
             machines[i] = machinesParent.transform.GetChild(i).gameObject;
+        }
+    }
+    private void FillWorkersArray()
+    {
+        workers = new GameObject[workersParent.transform.childCount];
+
+        for (int i = 0; i < workers.Length; i++)
+        {
+            Debug.Log(workersParent.transform.GetChild(i).gameObject + " is added.");
+            workers[i] = workersParent.transform.GetChild(i).gameObject;
         }
     }
 
@@ -36,6 +61,36 @@ public class IdleManager : MonoBehaviour
         GameObject newCostumer = Instantiate(costumer, costumerSpawnPoint.position, Quaternion.identity, costumersParent.transform);
         newCostumer.GetComponent<CostumerSc>().SendTo(costumerLastPoint.position - (Vector3.right * 1.5f * (costumerCount-1) ));
         AddToCostumersArray(newCostumer);
+    }
+
+    public void CostumerAsksFor(string productName, GameObject costumer)
+    {
+        switch (productName)
+        {
+            case "apple":
+                AvailableWorker().GetComponent<WorkerSc>().ServeToCostumer(costumer, machines[0]);
+                break;
+        }
+    }
+
+    private GameObject AvailableWorker()
+    {
+        GameObject availableWorker = new GameObject();
+        Debug.Log("AvailableWorker method 1");
+        
+        foreach (GameObject worker in workers)
+        {
+            if (worker.CompareTag("NotBusy"))
+            {
+                availableWorker = worker;
+            }
+            else
+            {
+                Debug.Log("No available worker.");
+            }
+        }
+
+        return availableWorker;
     }
 
     private void DeleteFromCostumersArray(GameObject deletedObj)
@@ -62,4 +117,9 @@ public class IdleManager : MonoBehaviour
         Debug.Log("Costumers is added to costumers array");
     }
 
+    // Reload the current scene to restart the game
+    public void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
 }
