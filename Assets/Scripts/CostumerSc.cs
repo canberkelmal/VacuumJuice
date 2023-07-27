587b31cca4acff50176644495d64ab81b78e8725
canberkelmal <elmalcanberk@gmail.com>
2023-07-27 23:59:13 +0300

    Costumer script is roughly ready.

--- Assets/Scripts/CostumerSc.cs
+++ Assets/Scripts/CostumerSc.cs
@@ -4,13 +4,17 @@ using UnityEngine;
 
 public class CostumerSc : MonoBehaviour
 {
+    public string askFor = "apple";
     public float movementSpeed = 1f;
+    public bool isWaiting = false;
+    
+
+    private IdleManager idleManager;
     private Vector3 destination = Vector3.zero;
 
-    // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        
+        idleManager = GameObject.Find("IdleManager").GetComponent<IdleManager>();
     }
 
     public void SendTo(Vector3 finalPoint)
@@ -24,7 +28,24 @@ public class CostumerSc : MonoBehaviour
         transform.position = Vector3.MoveTowards(transform.position, destination, movementSpeed * Time.deltaTime);
         if(transform.position == destination)
         {
+            Debug.Log("Reached to destination.");
+            idleManager.CostumerAsksFor(askFor, gameObject);
             CancelInvoke("GoToDestination");
+        }
+    }
+
+    public void TakeAndGo()
+    {
+        InvokeRepeating("GoToExit", 0, Time.deltaTime);
+    }
+
+    private void GoToExit()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, idleManager.costumerExitPoint.position, movementSpeed * Time.deltaTime);
+        if (transform.position == idleManager.costumerExitPoint.position)
+        {
+            Debug.Log("Costumer went.");
+            CancelInvoke("GoToExit");
         }
     }
 }
---
+++ Assets/Scripts/WorkerSc.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class WorkerSc : MonoBehaviour
+{
+    public float speed = 5f;
+
+
+    private IdleManager idleManager;
+    private GameObject machine, costumer;
+
+    private bool waitingCostumer = true;
+    private bool waitingForMachine = false;
+    private bool serving = false;
+    private bool goingMachine = false;
+    void Awake()
+    {
+        idleManager = GameObject.Find("IdleManager").GetComponent<IdleManager>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject == machine)
+        {
+            CancelInvoke("GoToMachine");
+            machine.GetComponent<MachineSc>().TakeProduct();
+            InvokeRepeating("GoToCostumer", 0, Time.deltaTime);
+        }
+    }
+
+    public void ServeToCostumer(GameObject costumer, GameObject machine)
+    {
+        this.costumer = costumer;
+        this.machine = machine;
+        StartDuty();
+    }
+
+    private void StartDuty()
+    {
+        if(machine.GetComponent<MachineSc>().status == 2)
+        {
+            InvokeRepeating("GoToMachine", 0, Time.deltaTime);
+        }
+    }
+
+    private void GoToMachine()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, machine.transform.Find("TakeProductPoint").position, speed * Time.deltaTime);
+    }
+
+    private void GoToCostumer()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, costumer.transform.position - Vector3.forward*3.5f, speed * Time.deltaTime);
+        if (transform.position == costumer.transform.position - Vector3.forward * 3.5f)
+        {
+            costumer.GetComponent<CostumerSc>().TakeAndGo();
+            CancelInvoke("GoToCostumer");
+        }
+    }
+
+
+}
