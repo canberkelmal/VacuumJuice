a16de18ef138356d8dfdca4aa8e21b4a9f9747c4
canberkelmal <elmalcanberk@gmail.com>
2023-08-16 22:23:55 +0300

    Machine upgrade cost and product prices are set due to machine level.

--- Assets/Scripts/MachineSc.cs
+++ Assets/Scripts/MachineSc.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -13,6 +14,10 @@ public class MachineSc : MonoBehaviour
     public Color readyColor, unreadyColor;
     public int machineLevel = 1;
     public float productPrice = 1;
+    public float firstLevelCost = 2;
+    public float levelCostConstan = 1.2571f;
+    public float firstLevelPrice = 6;
+    public float levelPriceConstan = 1.09f;
     public bool hasOwner = false;
 
     private Texture icon;
@@ -21,9 +26,51 @@ public class MachineSc : MonoBehaviour
     private GameObject statuIcon;
     private IdleManager idleManager;
 
+    int HesaplaKazanc(int level)
+    {
+        if (level >= 1 && level <= 9)
+        {
+            return level + 5;
+        }
+        else if (level >= 10 && level <= 25)
+        {
+            return 21 + (level - 10) * 3;
+        }
+        else if (level >= 26)
+        {
+            return 25 + (level - 25) * 7;
+        }
+
+        return 0; // Geçersiz level
+    }
+
+
+    int GetExpectedGain(int level)
+    {
+        int[] expectedGains = new int[]
+        {
+            6, 7, 8, 9, 10, 11, 12, 13, 14,
+            31, 34, 37, 40, 43, 46, 50, 54, 59, 63, 69,
+            74, 80, 86, 93, 202
+        };
+
+        if (level >= 1 && level <= 25)
+        {
+            return expectedGains[level - 1];
+        }
+        return 0; // Geçersiz level
+    }
+
     // Start is called before the first frame update
     void Start()
     {
+        for (int i = 1; i <= 25; i++)
+        {
+            int hesaplananKazanc = HesaplaKazanc(i);
+            int beklenenKazanc = GetExpectedGain(i);
+            Debug.Log($"Level {i} için hesaplanan kazanç: {hesaplananKazanc} | Beklenen: {beklenenKazanc}");
+        }
+
         idleManager = GameObject.Find("IdleManager").GetComponent<IdleManager>();
         fillImage = transform.Find("MachineCanvas").Find("BG").GetComponent<Image>();
         statuIcon = transform.Find("MachineCanvas").Find("Statu").gameObject;
@@ -34,6 +81,7 @@ public class MachineSc : MonoBehaviour
     public void InitMachine()
     {
         machineLevel = idleManager.GetMachineLevel(gameObject);
+        SetProductPrice();
         Transform obj = transform.Find("Obj");
         if (machineLevel == 0 )
         {
@@ -88,10 +136,26 @@ public class MachineSc : MonoBehaviour
     public void IncreaseMachineLevel(int addLevel)
     {
         idleManager.IncreaseMachineLevel(gameObject);
+        idleManager.SetMoneyCount(-firstLevelCost * MathF.Pow(levelCostConstan , (machineLevel + 1)));
+        SetProductPrice();
         machineLevel = idleManager.GetMachineLevel(gameObject);
 
         idleManager.machinePanel.transform.Find("LevelTX").GetComponent<Text>().text = "Level " + machineLevel.ToString();
         InitMachine();
+    }
+
+    void SetProductPrice()
+    {
+        productPrice = firstLevelPrice * (MathF.Pow(levelPriceConstan, machineLevel));
+        if(machineLevel >= 10 && machineLevel<25)
+        {
+            productPrice *= 2;
+        }
+        else if (machineLevel >= 25)
+        {
+            productPrice *= 4;
+        }
+        productPrice = (int)productPrice;
     }
 
     public void PrepareTest()
